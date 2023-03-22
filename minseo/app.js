4cea54611e3e074aa2ef448833102ac0aec4ed15
김민서 <gimminseo@minseo-mac.local>
2023-03-22 17:51:14 +0900

    [김민서] 초기세팅

---
+++ minseo/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+const { DataSource } = require("typeorm");
+
+const app = express();
+const PORT = process.env.PORT;
+
+const appDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initalized!");
+  })
+  .catch((err) => {
+    console.log("Error during Data source initialization", err);
+    appDataSource.destroy();
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//health check
+app.get("/ping", function (req, res, next) {
+  res.json({ message: "pong" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
