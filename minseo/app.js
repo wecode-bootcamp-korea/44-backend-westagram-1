66fc298d0ee61d00cc54a6c6efd4712e2b2f6556
김민서 <gimminseo@minseo-mac.local>
2023-03-22 22:00:24 +0900

    [signup]

--- minseo/app.js
+++ minseo/app.js
@@ -35,6 +35,20 @@ app.use(morgan("tiny"));
 app.get("/ping", function (req, res, next) {
   res.json({ message: "pong" });
 });
+//회원가입
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  await appDataSource.query(
+    `INSERT INTO users(
+    name,
+    email,
+    password
+  )VALUES(?, ?, ?);`,
+    [name, email, password]
+  );
+  res.status(201).json({ message: "userCreated" });
+});
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
