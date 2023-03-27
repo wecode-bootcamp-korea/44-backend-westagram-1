53de200eac4221e7014128c0e1dd4fa97c95744d
Mac <mac@Macui-MacBookPro.local>
2023-03-27 16:30:55 +0900

    layerd

--- seyik/models/likeDao.js
+++ seyik/models/likeDao.js
@@ -1,25 +1,6 @@
 //model/likeDao.js
 
-const { DaoSource, DataSource } = require("typeorm");
-
-const appDataSource = new DataSource({
-  type: process.env.DB_CONNECTION,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error occurred during Data Source initialization", err);
-    appDataSource.destroy();
-  });
+const appDataSource = require("./appDataSource");
 
 const createLikes = async (userId, postId) => {
   try {
