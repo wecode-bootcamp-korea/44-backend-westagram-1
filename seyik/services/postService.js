dc83909870fea912b93d9c166520064bf1b1623e
Mac <mac@Macui-MacBookPro.local>
2023-03-27 16:52:18 +0900

    layerd

--- seyik/services/postService.js
+++ seyik/services/postService.js
@@ -2,7 +2,19 @@
 
 const postDao = require("../models/postDao");
 
-const register = async (title, content, userId) => {
+const register1234 = async (title, content, userId) => {
+  const createPost = await postDao.createPost(title, content, userId);
+
+  return createPost;
+};
+
+const register123 = async (title, content, userId) => {
+  const createPost = await postDao.createPost(title, content, userId);
+
+  return createPost;
+};
+
+const register12 = async (title, content, userId) => {
   const createPost = await postDao.createPost(title, content, userId);
 
   return createPost;
