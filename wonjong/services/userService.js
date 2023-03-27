8c3c48e73195c975a490a177e83b68e8a50edd09
naturerest <asdqwe747@gmail.com>
2023-03-27 17:14:13 +0900

    Layered Pattern-수정

--- wonjong/services/userService.js
+++ wonjong/services/userService.js
@@ -3,16 +3,17 @@ const userDao = require('../models/userDao');
 const signUp = async (name, email, password, profileImage) => {
   // password validation using REGEX
   const pwValidation = new RegExp('^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})');
-
   const emailValidation = new RegExp(
     "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
   );
+
   if (!pwValidation.test(password) || !emailValidation.test(email)) {
     const err = new Error('PASSWORD_OR_EMAIL_NOT_VALID');
     console.log(err);
     err.statusCode = 409;
     throw err;
   }
+
   const createUser = await userDao.createUser(name, email, password, profileImage);
 
   return createUser;
