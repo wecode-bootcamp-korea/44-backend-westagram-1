f59a9c88f852971b5a3aa519966f83cc6284acde
Mac <mac@Macui-MacBookPro.local>
2023-03-27 16:50:42 +0900

    layerd

--- seyik/models/postDao.js
+++ seyik/models/postDao.js
@@ -47,6 +47,40 @@ const allPost = async (
   }
 };
 
+const userPost = async (
+  userId,
+  userProfileImage,
+  postingId,
+  postingImageUrl,
+  postingContent
+) => {
+  try {
+    return await appDataSource.query(
+      `SELECT 
+  users.id as userId,
+  users.profile_image as userProfileImage,
+  (SELECT
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          "postingId", posts.id,
+          "postingImageUrl", posts.title,
+          "postingContent", posts.content
+        )
+      )
+      )as postings
+    FROM users
+    JOIN posts 
+    ON users.id = posts.user_id
+    WHERE posts.user_id = ?
+    `,
+      [userId, userProfileImage, postingId, postingImageUrl, postingContent]
+    );
+  } catch (err) {
+    const error = new Error("INVALID_DATA_SELECT");
+    error.statusCode = 500;
+    throw error;
+  }
+};
 module.exports = {
   createPost,
   allPost,
