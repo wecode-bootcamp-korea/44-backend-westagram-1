cc611da99235d72faee6b2ce3a3e5140899c6073
김민서 <gimminseo@minseo-mac.local>
2023-03-27 14:50:40 +0900

    [fix]layered - pattern

---
+++ minseo/models/likesDao.js
@@ -0,0 +1,22 @@
+const appDataSource = require('./appDataSource');
+
+const createlikes = async (userId, postId) => {
+  try {
+    return await appDataSource.query(
+      `INSERT INTO likes(
+          user_id,
+          post_id
+          )VALUES(?,?);
+        `,
+      [userId, postId]
+    );
+  } catch (err) {
+    const error = new Error('INVALID_DATA_INPUT_LIKES');
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
+module.exports = {
+  createlikes,
+};
