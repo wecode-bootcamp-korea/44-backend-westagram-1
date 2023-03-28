6d4e25fa064660633f8d0ffef95c67d690bd9e20
김민서 <gimminseo@minseo-mac.local>
2023-03-28 16:27:47 +0900

    bcrypt

--- minseo/service/userService.js
+++ minseo/service/userService.js
@@ -3,17 +3,19 @@ const bcrypt = require('bcrypt');
 const saltRounds = 8;
 
 const signUp = async (name, email, password) => {
-  const pwValidation = new RegExp('^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$');
-
-  const emailValidation = new RegExp(
-    '/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i'
+  const pwValidation = new RegExp(
+    '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})'
   );
 
-  /* (!pwValidation.test(password) || !emailValidation.test(email)) {
+  const emailValidation = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+
+  if (!pwValidation.test(password) || !emailValidation.test(email)) {
     const err = new Error('PASSWORD_OR_EMAIL_IS_NOT_VALID');
     err.statusCode = 409;
     throw err;
-  }*/
+  }
   const makeHash = async (pass, saltRounds) => {
     return await bcrypt.hash(pass, saltRounds);
   };
