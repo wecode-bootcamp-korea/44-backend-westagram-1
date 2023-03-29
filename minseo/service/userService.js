da078f658d1c7896e84a516ffa7e2dcd679781a1
김민서 <gimminseo@minseo-mac.local>
2023-03-29 10:19:50 +0900

    [fixed1]jwtToken

--- minseo/service/userService.js
+++ minseo/service/userService.js
@@ -3,29 +3,24 @@ const bcrypt = require('bcrypt');
 const saltRounds = 8;
 const jwt = require('jsonwebtoken');
 
-const signUp = async (name, email, password, profileImage) => {
+const signUp = async (name, email, password) => {
   const pwValidation = new RegExp(
     '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})'
   );
 
   const emailValidation = new RegExp(
-    '/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i'
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
   );
 
   if (!pwValidation.test(password) || !emailValidation.test(email)) {
     const err = new Error('PASSWORD_OR_EMAIL_IS_NOT_VALID');
-    err.statusCode = 400;
+    err.statusCode = 409;
     throw err;
   }
 
   const hashPassword = bcrypt.hashSync(password, saltRounds);
 
-  const createUser = await userDao.createUser(
-    name,
-    email,
-    hashPassword,
-    profileImage
-  );
+  const createUser = await userDao.createUser(name, email, hashPassword);
   return createUser;
 };
 
@@ -34,7 +29,7 @@ const signIn = async (email, password) => {
   const match = await bcrypt.compare(password, user.password);
   if (match) {
     const payLoad = { email: email };
-    const secretKey = 'mySercetKey';
+    const secretKey = process.env.SECRETKEY;
     const jwtToken = jwt.sign(payLoad, secretKey);
 
     return jwtToken;
